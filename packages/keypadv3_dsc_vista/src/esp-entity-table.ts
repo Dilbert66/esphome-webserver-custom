ca88d1c26fb3a8cd7c78cc497ea4d380143d379f
Dilbert66 <aturbide99@gmail.com>
2026-01-05 10:15:27 -0500

    - removed test entry

--- packages/keypadv3_dsc_vista/src/esp-entity-table.ts
+++ packages/keypadv3_dsc_vista/src/esp-entity-table.ts
@@ -63,7 +63,7 @@ export const stateOff = "OFF";
 export function getBasePath() {
   let str = window.location.pathname;
    //return "http://dscalarm2040.local";
-  return "http://dscalarm8266.local";
+  //return "http://dscalarm8266.local";
   //return "http://vistaalarmesp32idf.local";
   return str.endsWith("/") ? str.slice(0, -1) : str;
 }
